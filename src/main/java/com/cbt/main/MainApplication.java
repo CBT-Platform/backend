06004911b84f06ee75721b9c15a48ccadbeb3ac0
saraswatikumari <saraswati.kumari@oracle.com>
2025-03-27 01:28:50 +0530

    added PingController

--- src/main/java/com/cbt/main/MainApplication.java
+++ src/main/java/com/cbt/main/MainApplication.java
@@ -2,8 +2,10 @@ package com.cbt.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.cbt.controller"})
 public class MainApplication {
 
 	public static void main(String[] args) {
